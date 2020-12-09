81860979a4e5d24a20cd34f01f39d4815d18d6e5
Tomaz Mazej <tomaz.mazej@gmail.com>
2020-12-09 21:05:23 +0100

    First commit

--- StarWars-Soundboard/ViewController.swift
+++ StarWars-Soundboard/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let soundFileNames = ["VaderBreathing", "ChewyRoar", "Jabba", "R2D2"]
+    var audioPlayers = [AVAudioPlayer]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for sound in soundFileNames{
+            do{
+                let url = NSURL(fileURLWithPath: Bundle.main.path(forResource: sound, ofType: "wav")!)
+                let audioPlayer = try AVAudioPlayer(contentsOf: url as URL)
+                
+                audioPlayers.append(audioPlayer)
+            }
+            catch{
+                audioPlayers.append(AVAudioPlayer())
+            }
+        }
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let audioPlayer = audioPlayers[sender.tag]
+        audioPlayer.play()
+    }
 }
 
